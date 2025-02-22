2472e40628a8b9f59e8bf0572a27aff08085a2c5
Roberto Esteves <contact@robertoesteves.dev>
2025-02-22 16:27:35 -0300

    setup air and css reset

--- cmd/web/uno.config.ts
+++ cmd/web/uno.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig, presetWind3 } from 'unocss'
+import { defineConfig, presetAttributify, presetWind3 } from 'unocss'
 import fs from 'fs';
 
 const content = fs.readFileSync('./node_modules/@unocss/reset/tailwind.css').toString();
 
 export default defineConfig({
   presets: [
-    presetWind3()
+    presetWind3(),
+    presetAttributify(),
   ],
   preflights: [
     {
